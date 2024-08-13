71734276130434e5cc667400809c03318a4a550a
PRINCE GDS <112704679+PRINCE-GDS@users.noreply.github.com>
2024-08-13 08:55:37 +0500

    𝑷-𝑴𝑫

--- plugins/dl-play2.js
+++ plugins/dl-play2.js
@@ -5,7 +5,7 @@ import {youtubedl, youtubedlv2} from '@bochilteam/scraper';
 import ytdl from 'ytdl-core';
 import {bestFormat, getUrlDl} from '../lib/y2dl.js';
 import YTDL from "../lib/ytdll.js";
-import fs from "fs";
+import fs from "fs-extra";
 let limit1 = 100;
 let limit2 = 400;
 let limit_a1 = 50;

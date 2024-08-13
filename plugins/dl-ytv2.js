419757d2d13d3b0218118253e695d0550cedb4a5
PRINCE GDS <112704679+PRINCE-GDS@users.noreply.github.com>
2024-08-13 13:09:11 +0500

    𝑷-𝑴𝑫

--- plugins/dl-ytv2.js
+++ plugins/dl-ytv2.js
@@ -1,7 +1,7 @@
 import {youtubedl, youtubedlv2} from '@bochilteam/scraper';
 import fetch from 'node-fetch';
 const handler = async (m, {conn, args}) => {
-  if (!args[0]) throw '*[🛡️]Past a Youtube link...*';
+  if (!args[0]) throw '*🚩Need a Youtube Link...*';
   await m.reply(wait);
     m.react(rwait);
   try {
@@ -12,7 +12,7 @@ const handler = async (m, {conn, args}) => {
     const dl_url = await yt.video[q].download();
     const ttl = await yt.title;
     const size = await yt.video[q].fileSizeH;
-    const cap = `*◉╭━⊱⌈📥 𝐘𝐎𝐔𝐓𝐔𝐁𝐄 𝐃𝐋 📥⌋⊱━╮◉*\n🪩*Title:* ${ttl}\n🪩*Size:* ${size}`.trim();
+    const cap = `*◉╭━⊱⌈📥 𝙔𝙊𝙐𝙏𝙐𝘽𝙀_𝘿𝙇 📥⌋⊱━╮◉*\n🎉*${mssg.title}:* ${ttl}\n🌐*${mssg.size}:* ${size}`.trim();
     await await conn.sendMessage(m.chat, {document: {url: dl_url}, caption: cap, mimetype: 'video/mp4', fileName: ttl + `.mp4`}, {quoted: m});
   } catch {
     m.react(done)
@@ -22,14 +22,14 @@ const handler = async (m, {conn, args}) => {
       const n = lolh.result.title || 'error';
       const n2 = lolh.result.link;
       const n3 = lolh.result.size;
-      const cap2 = `*◉╭━⊱⌈📥 𝐘𝐎𝐔𝐓𝐔𝐁𝐄 𝐃𝐋 📥⌋⊱━╮◉*\n🪩*Title:* ${n}\n🪩*Size:* ${n3}`.trim();
+      const cap2 = `*◉╭━⊱⌈📥 𝙔𝙊𝙐𝙏𝙐𝘽𝙀_𝘿𝙇 📥⌋⊱━╮◉*\n🎉*${mssg.title}:* ${n}\n🌐*${mssg.size}:* ${n3}`.trim();
       await conn.sendMessage(m.chat, {document: {url: n2}, caption: cap2, mimetype: 'video/mp4', fileName: n + `.mp4`}, {quoted: m});
     } catch {
       m.react(done)
-      await conn.reply(m.chat, '*[⛔]error couldnt download the video*', m);
+      await conn.reply(m.chat, '*Error couldnt download the video*', m);
     }
   }
 };
-handler.command = /^ytmp4doc|ytvdoc|ytmp4.2|ytv.2|ytdl$/i;
+handler.command = /^ytmp4doc|ytvdoc|ytdl$/i;
 handler.tags = ['downloader'];
 export default handler;

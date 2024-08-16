4447a140499ea774617b00120cc4e77e94e67fd3
PRINCE GDS <112704679+PRINCE-GDS@users.noreply.github.com>
2024-08-16 10:05:43 +0500

    𝑷-𝑴𝑫

--- plugins/dl-ytv.js
+++ plugins/dl-ytv.js
@@ -1,77 +1,56 @@
-import fs from 'fs';
-import path from 'path';
-import ytdl from 'ytdl-core';
-import { Client } from 'undici';
-import { fileURLToPath } from 'url';
-import fetch from 'node-fetch';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
+import fg from 'api-dylux'
+import { youtubedl, youtubedlv2 } from '@bochilteam/scraper-sosmed'
+let limit = 320
 let handler = async (m, { conn, args, isPrems, isOwner, usedPrefix, command }) => {
-  let chat = global.db.data.chats[m.chat];
-  if (!args || !args[0]) throw `🚩 ${mssg.example}:\n${usedPrefix + command} past your Youtube link`;
-  if (!args[0].match(/youtu/gi)) throw `❎ Verify that the YouTube link`;
-  await m.react('⏳')
+	if (!args || !args[0]) throw `✳️ ${mssg.example} :\n${usedPrefix + command} past your Yt link here`
+    if (!args[0].match(/youtu/gi)) throw `❎ ${mssg.noLink('YouTube')}`
+	 let chat = global.db.data.chats[m.chat]
+	 m.react(rwait) 
+	
+	 let q = args[1] || '360p'
+ try {
+		const yt = await fg.ytv(args[0], q)
+		let { title, dl_url, quality, size, sizeB } = yt
+        let isLimit = limit * 1024 < sizeB 
 
-  const videoDetails = await ytddl(args[0]);
-  if (!videoDetails) throw `❎ Error downloading video`;
-
-  const { url, title, author, description } = videoDetails;
-
-  const response = await fetch(url);
-  const data = await response.buffer();
-
-  const caption = `╭━━━━⊱𝗬𝗢𝗨𝗧𝗨𝗕𝗘⊱━━━━𓅓
-🚀 Author: ${author || 'Unknown'}	  
-🎉 ${mssg.title}: ${title || 'Unknown'}
-📃 ${mssg.desc}: ${description || 'No description available'}
-🖇️ ${mssg.link}: ${args[0]}
-╰━━━━━𝗣-𝗠𝗗━━━━━━𓅓`;
-
-  conn.sendFile(m.chat, data, `${title || 'video'}.mp4`, caption, m, false, { asDocument: chat.useDocument });
-  await m.react('✅')
-};
-
-
-handler.help = ['ytmp4 <yt-link>'];
-handler.tags = ['downloader'];
-handler.command = ['ytmp4', 'ytv'];
-handler.diamond = false;
-
-export default handler;
-
-async function getCookies() {
-  const cookiesPath = path.resolve(__dirname, '../Assets/cookies.json');
-  if (!fs.existsSync(cookiesPath)) {
-    throw new Error('Cookies file not found');
-  }
-  return JSON.parse(fs.readFileSync(cookiesPath, 'utf-8'));
+     await conn.loadingMsg(m.chat, '📥 Downloading', ` ${isLimit ? `≡  *𝗬𝗢𝗨𝗧𝗨𝗕𝗘*\n\n▢ *⚖️${mssg.size}*: ${size}\n▢ *🎞️${mssg.quality}*: ${quality}\n\n▢ _${mssg.limitdl}_ *+${limit} MB*` : '✅ Download Completed' }`, ["▬▭▭▭▭▭", "▬▬▭▭▭▭", "▬▬▬▭▭▭", "▬▬▬▬▭▭", "▬▬▬▬▬▭", "▬▬▬▬▬▬"], m)
+     
+	  if(!isLimit) conn.sendFile(m.chat, dl_url, title + '.mp4', `
+ 
+╭━━━━⊱𝗬𝗢𝗨𝗧𝗨𝗕𝗘⊱━━━━𓅓 
+*📌${mssg.title}:* ${title}
+*🎞️${mssg.quality}:* ${quality}
+*⚖️${mssg.size}:* ${size}
+╰━━━━━𝗣-𝗠𝗗━━━━━━𓅓`.trim(), m, false, { asDocument: chat.useDocument })
+		m.react(done) 
+ 	} catch {
+ 	
+	try {
+	let yt = await fg.ytmp4(args[0], q)
+    let { title, size, sizeB, dl_url, quality } = yt
+  
+  let isLimit = limit * 1024 < sizeB 
+ 
+  await conn.loadingMsg(m.chat, '📥 Downloading', ` ${isLimit ? `≡  *𝗬𝗢𝗨𝗧𝗨𝗕𝗘*\n\n▢ *⚖️${mssg.size}*: ${size}\n▢ *🎞️${mssg.quality}*: ${quality}\n\n▢ _${mssg.limitdl}_ *+${limit} MB*` : '✅ Download Completed' }`, ["▬▭▭▭▭▭", "▬▬▭▭▭▭", "▬▬▬▭▭▭", "▬▬▬▬▭▭", "▬▬▬▬▬▭", "▬▬▬▬▬▬"], m)
+	  
+if(!isLimit) conn.sendFile(m.chat, dl_url, title + '.mp4', `
+ ≡  *FG YTDL 2*
+  
+▢ *📌${mssg.title}* : ${title}
+*🎞️${mssg.quality}:* ${quality}
+▢ *⚖️${mssg.size}* : ${size}
+`.trim(), m, false, { asDocument: chat.useDocument })
+		m.react(done)
+		
+	} catch {
+		await m.reply(`❎ ${mssg.error}`)
+	}
+		} 
 }
+handler.help = ['ytmp4 <link yt>']
+handler.tags = ['dl'] 
+handler.command = ['ytmp4', 'fgmp4', 'ytv']
+handler.diamond = false
 
-async function createClient() {
-  const cookies = await getCookies();
-  return new Client("https://www.youtube.com", {
-    headers: {
-      "Cookie": cookies.map(cookie => `${cookie.name}=${cookie.value}`).join('; ')
-    }
-  });
-}
-
-async function ytddl(url) {
-  try {
-    const client = await createClient();
-    const yt = await ytdl.getInfo(url, { requestOptions: { client: client } });
-    const link = ytdl.chooseFormat(yt.formats, { quality: 'highest', filter: 'audioandvideo' });
-
-    return {
-      url: link.url,
-      title: yt.videoDetails.title,
-      author: yt.videoDetails.author.name,
-      description: yt.videoDetails.description,
-    };
-  } catch (error) {
-    console.error("An error occurred:", error);
-    return null;  // Ensure a null is returned on error
-  }
-}
+export default handler

c61cec02cf1b74b2092e262a04c5fc207d155c7d
Mselachui03 <148160583+Mselachui03@users.noreply.github.com>
2024-08-18 15:45:09 +0300

    Update main-botInfo.js

--- plugins/main-botInfo.js
+++ plugins/main-botInfo.js
@@ -56,11 +56,11 @@ let infobt = `
 ▢ *${chats.length}* Total Chats
 
 *≡ OWNER🧑‍💻*
- _PRINCE GDS_
-▢ https://whatsapp.com/channel/0029VaKNbWkKbYMLb61S1v11
+ _MSELACHUI_
+▢ https://whatsapp.com/channel/0029VakhqAaLtOjBJOL9Wn1q
   
 ▢ MY BOT GC LINK.
-  https://chat.whatsapp.com/Jo5bmHMAlZpEIp75mKbwxP
+  https://chat.whatsapp.com/KStfBpcFDOi154eVtrz6n1
 
  *≡ S E R V E R*
 *🛑 RAM:* ${format(totalmem() - freemem())} / ${format(totalmem())}
